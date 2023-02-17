8a01177a5119402dc3b2a8735dca0a176bd24b90
Dimas Sulz <dimassulz@gmail.com>
2023-02-17 12:57:33 -0300

    add botao voltar, mensagens de sucesso/erro e lista de produtos como tabela

--- aula17/script.js
+++ aula17/script.js
@@ -9,76 +9,135 @@
 
 
 Atividade secundária
-1 - Limitar quantidade de caracteres do nome e descrição
+1 - Limitar quantidade de caracteres do nome e descrição (maxlength)
 2 - Realmente quer concluir o cadastramento
 3 - Validar se o código digitado já existe
 */
 
 /* Identificar os elementos no html através do ID */
-let codigoProduto = document.getElementById('codigo-produto')
-let nomeProduto = document.querySelector('#nome-produto')
-let descricaoProduto = document.getElementById('descricao-produto')
-let precoProduto = document.querySelector('#preco-produto')
-let botaoSalvar = document.getElementById('botao-salvar')
-let botaoConcluir = document.getElementById('botao-concluir')
-let sectionCadastramento = document.getElementById('cadastramento-produtos')
-let sectionListaProdutos = document.getElementById('listagem-produtos')
-let formulario = document.querySelector('form')
+let codigoProduto = document.getElementById("codigo-produto");
+let nomeProduto = document.querySelector("#nome-produto");
+let descricaoProduto = document.getElementById("descricao-produto");
+let precoProduto = document.querySelector("#preco-produto");
+let botaoSalvar = document.getElementById("botao-salvar");
+let botaoConcluir = document.getElementById("botao-concluir");
+let sectionCadastramento = document.getElementById("cadastramento-produtos");
+let sectionListaProdutos = document.getElementById("listagem-produtos");
+let formulario = document.querySelector("form");
+let mensagemSucesso = document.querySelector("#mensagemSucesso");
+let mensagemErro = document.querySelector("#mensagemErro");
+let listaProdutosCadastrados = document.querySelector("#lista-produtos tbody");
+let botaoVoltar = document.querySelector("#voltar");
 
 /* Variaveis de armazenamento */
 let produto = {
-  codigo:0,
-  nome: '',
-  descricao:'',
-  preco:0
-}
+  codigo: 0,
+  nome: "",
+  descricao: "",
+  preco: 0,
+};
 
-let listaProdutos = []
+//Lista de produtos vazia
+let listaProdutos = [];
 
 /* Verificar se todos os campos do form estão preenchidos */
-function camposPreenchidos () {
+function camposPreenchidos() {
   // Sim ou não // True or False
   if (
-    codigoProduto.value !== '' &&
-    nomeProduto.value !== '' &&
-    descricaoProduto.value !== '' &&
-    precoProduto.value !== ''
-  ) { // Campos preenchidos
-    return true
-  } else { // Senao
-    return false
+    codigoProduto.value !== "" &&
+    nomeProduto.value !== "" &&
+    descricaoProduto.value !== "" &&
+    precoProduto.value !== ""
+  ) {
+    // Campos preenchidos
+    return true;
+  } else {
+    // Senao
+    return false;
   }
 }
 
 /* Fazendo do jeito pedreiro master  fins didáticos*/
-function liparFormulario(){
-  codigoProduto.value = ''
-  nomeProduto.value = ''
-  descricaoProduto.value = ''
-  precoProduto.value = ''
+function liparFormulario() {
+  codigoProduto.value = "";
+  nomeProduto.value = "";
+  descricaoProduto.value = "";
+  precoProduto.value = "";
 }
 
-function salvarProduto (){
+function salvarProduto() {
   // Crio uma cópia do modelo do objeto  produto
-  let produtoCriado = {...produto}
+  let produtoCriado = { ...produto };
   // Atribuo valor as propriedades desse objeto
-  produtoCriado.codigo = codigoProduto.value
-  produtoCriado.nome = nomeProduto.value
-  produtoCriado.descricao = descricaoProduto.value
-  produtoCriado.preco = precoProduto.value
+  produtoCriado.codigo = codigoProduto.value;
+  produtoCriado.nome = nomeProduto.value;
+  produtoCriado.descricao = descricaoProduto.value;
+  produtoCriado.preco = precoProduto.value;
 
   // Enviando pro array que armazena os produtos
-  listaProdutos.push(produtoCriado)
+  listaProdutos.push(produtoCriado);
   // Limpando o form
-  formulario.reset() // Forma fácil de fazer se souber pesquisar antes do professor ensinar
+  formulario.reset(); // Forma fácil de fazer se souber pesquisar antes do professor ensinar
   //liparFormulario() // Pedreiro master fins didáticos
-  console.log(listaProdutos)
+  exibirMensagemSucesso();
 }
 
-botaoSalvar.onclick = () => {
-  if (camposPreenchidos()) { // Se for true
-    salvarProduto()
-  } else { // senao 
-    console.log('Campos não preenchidos')
-  }
+function exibirMensagemSucesso() {
+  mensagemErro.style.display = "none";
+  mensagemSucesso.style.display = "block";
+  mensagemSucesso.innerHTML = `Produto salvo com sucesso!`;
 }
+
+function exibirMensagemErro() {
+  mensagemSucesso.style.display = "none";
+  mensagemErro.style.display = "block";
+  mensagemErro.innerHTML = `Campos não preenchidos!`;
+}
+
+/*
+ocultar o formulario
+inserir na lista os produtos
+mostrar a lista
+*/
+const concluirCadastramento = () => {
+  listaProdutosCadastrados.innerHTML = ""; //limpar toda a lista
+  for (let produto of listaProdutos) {
+    listaProdutosCadastrados.innerHTML += `
+    <tr>
+      <td>${produto.codigo}</td>
+      <td>${produto.nome}</td>
+      <td>${produto.descricao}</td>
+      <td>${produto.preco}</td>
+    </tr>
+    `;
+  }
+  mostrarListaDeProdutos();
+};
+
+const mostrarListaDeProdutos = () => {
+  sectionCadastramento.style.display = "none"; //ocultar o form
+  sectionListaProdutos.style.display = "block"; //mostrando a lista
+};
+
+const mostrarFormulario = () => {
+  sectionCadastramento.style.display = "block"; //mostrar o form
+  sectionListaProdutos.style.display = "none"; //ocultando a lista
+};
+
+botaoSalvar.onclick = () => {
+  if (camposPreenchidos()) {
+    // Se for true
+    salvarProduto();
+  } else {
+    // senao
+    exibirMensagemErro();
+  }
+};
+
+botaoConcluir.onclick = () => {
+  concluirCadastramento();
+};
+
+botaoVoltar.onclick = () => {
+  mostrarFormulario();
+};
